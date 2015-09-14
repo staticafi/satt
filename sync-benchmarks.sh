65d3ef92d5cf1afc93dc991b5af7eb7568dfa5ec
Statica project <statica@arran.fi.muni.cz>
2015-09-14 08:47:00 +0200

    update sync-benchmarks.sh
    Do not checkout our repository, but use the official one

--- sync-benchmarks.sh
+++ sync-benchmarks.sh
@@ -6,18 +6,20 @@ sync_benchmarks()
 	MACHINE="$1"
 	REMOTE_DIR="$2"
 	BENCHMARKS_DIR="$3"
+	YEAR="$4"
 
-	USER=${MACHINE%%@*}
-	HOST=`hostname`
-	GIT_REP="ssh://$USER@$HOST/$BENCHMARKS_DIR"
+	GIT_REP="https://github.com/dbeyer/sv-benchmarks"
 
 	ssh "$MACHINE"\
 		"cd ${REMOTE_DIR};\
 		 if cd benchmarks &>/dev/null;\
 			then (git fetch origin master &&\
 			      git reset --hard origin/master &&\
-			      git clean -xdf)&>/dev/null;\
-			else git clone $GIT_REP; fi"
+			      git clean -xdf &&\
+			      git checkout $YEAR)&>/dev/null;\
+			else git clone $GIT_REP benchmarks &&
+				git checkout $YEAR;\
+		 fi"
 
 	return $?
 }
@@ -25,7 +27,8 @@ sync_benchmarks()
 MACHINE="$1"
 REMOTE_DIR="$2"
 BENCHMARKS_DIR="$3"
+YEAR="$4"
 
-sync_benchmarks "$MACHINE" "$REMOTE_DIR" "$BENCHMARKS_DIR" || exit 1
+sync_benchmarks "$MACHINE" "$REMOTE_DIR" "$BENCHMARKS_DIR" "$YEAR" || exit 1
 
 exit 0
