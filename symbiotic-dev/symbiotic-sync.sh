e4475e22df9a97cb498f425491dce73a4c77ff08
Statica project <statica@arran.fi.muni.cz>
2015-10-06 14:01:20 +0200

    add symbiotic-dev tool
    This is a developement version of the symbiotic tool

---
+++ symbiotic-dev/symbiotic-sync.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Sync script that satt needs for running Symbiotic
+
+sync_symbiotic()
+{
+	MACHINE="$1"
+	REMOTE_DIR="$2"
+	SYMBIOTIC_DIR="$3"
+
+	USER=${MACHINE%%@*}
+	HOST=`hostname`
+	GIT_REP="ssh://$USER@$HOST/$SYMBIOTIC_DIR"
+
+	ssh "$MACHINE"\
+		"cd ${REMOTE_DIR};\
+		 if cd symbiotic-dev &>/dev/null;\
+			then git pull &>/dev/null;\
+			else git clone $GIT_REP; fi"
+
+	return $?
+}
+
+sendfile()
+{
+	FILE="$1"
+	rsync -r "$FILE" "$MACHINE":"$REMOTE_DIR"/satt/symbiotic-dev/ || exit 1
+}
+
+MACHINE="$1"
+REMOTE_DIR="$2"
+SYMBIOTIC_DIR="$3"
+
+sendfile symbiotic-dev/run_benchmark
+
+sync_symbiotic "$MACHINE" "$REMOTE_DIR" "$SYMBIOTIC_DIR" || exit 1
+
+exit 0
